1ad6f1e759300704f6acc48ba01950847955e64b
Dima <75967292+DukaDima@users.noreply.github.com>
2022-06-13 11:14:46 +0300

    created next poject

---
+++ global-components/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Button from '../components/Button/Button'
+
+const Home: NextPage = () => {
+  return <>
+  Hello
+
+  <Button/>
+  
+  </>
+
+}
+export default Home
