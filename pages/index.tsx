898a23dc55d2c58d5a3734e77b4b0f82f6710512
Dima <75967292+DukaDima@users.noreply.github.com>
2022-06-20 16:35:44 +0300

    created Button component without styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import type { NextPage } from 'next'
-import Button from '../components/Button/Button'
+import Button from '../components/Button'
 
 const Home: NextPage = () => {
   return <>
- <h1> Hello</h1>
+    <h1> Hello</h1>
 
-  <Button 
-  href='/'
-  text='login'
-  
-
-
-
+    <Button
+      href='/'
+      text='login'
+      className=''
     />
-  
   </>
 
 }
